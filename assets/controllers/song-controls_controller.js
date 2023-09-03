8f38273f6e024d32a4b892fdaab9ade84af815fc
Bartosz Kubek <bartosz.kubek@gmail.com>
2023-09-03 11:38:34 +0200

    1.20 Using Stimulous (Contorller + connecting with Twig) and Axios for XHR

---
+++ assets/controllers/song-controls_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus";
+import axios from "axios";
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    // connect() {
+    //     this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+    // }
+
+    static values = {
+        infoUrl: String,
+    };
+
+    play(event) {
+        event.preventDefaults;
+
+        axios.get(this.infoUrlValue).then((response) => {
+            console.log(response);
+            let audio = new Audio(response.data.url);
+            audio.play();
+        });
+    }
+}
